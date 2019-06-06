2522a70c5264eba3913e0d769f27806e01c7535a
Herman Rivas <hrivas5726@aol.com>
2019-06-06 15:20:03 -0400

    buttons

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,25 @@ import hogs from '../porkers_data';
 import Hogs from './Hogs.js'
 
 class App extends Component {
+
+  state ={
+    hogs: hogs
+  }
+
+  sortOrFilter = (e) => {
+
+
+  }
+
   render() {
     return (
       <div className="App">
           < Nav />
+            <div onClick={this.sortOrFilter}>
+              <button> Sort By Name</button>
+              <button> Sort By Weight</button>
+              <button> Greased</button>
+            </div>
           < Hogs hogs={hogs}/>
 
       </div>
--- src/components/Hog.js
+++ src/components/Hog.js
@@ -21,11 +21,11 @@ export default class Hog extends Component {
       return <div className="smallHeader">
       Specialty: {this.props.hog.specialty}
       <p></p>
-      Greased: {this.props.hog.greased}
+      Greased: {this.props.hog.greased ? "yes" : "no"}
       <p></p>
       Weight as a ratio: {this.props.hog['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']}
       <p></p>
-      Highest medal achieved: 
+      Highest medal achieved:
       {this.props.hog['highest medal achieved']}
       </div>
     }
