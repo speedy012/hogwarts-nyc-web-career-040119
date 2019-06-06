03a7f968ca905db02af319441b353e0218ae3b24
dnj547 <dnj547@live.com>
2019-06-06 16:25:49 -0400

    Sort by name and weight

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,22 @@ class App extends Component {
   }
 
   sortOrFilter = (e) => {
+    // console.log(e.target.id)
+    if (e.target.id === 'name') {
+      const newHogs = this.state.hogs.sort((a,b) => a.name.localeCompare(b.name));
+      this.setState({
+        hogs: newHogs
+      })
+    } else if (e.target.id === 'weight') {
+      const weight = 'weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'
+      const newHogs = this.state.hogs.sort((a,b) => a[weight] - b[weight])
+      
+      this.setState({
+        hogs: newHogs
+      })
+    } else if (e.target.id === 'greased') {
 
+    }
 
   }
 
@@ -20,11 +35,11 @@ class App extends Component {
       <div className="App">
           < Nav />
             <div onClick={this.sortOrFilter}>
-              <button> Sort By Name</button>
-              <button> Sort By Weight</button>
-              <button> Greased</button>
+              <button id='name'> Sort By Name</button>
+              <button id='weight'> Sort By Weight</button>
+              <button id='greased'> Greased</button>
             </div>
-          < Hogs hogs={hogs}/>
+          < Hogs hogs={this.state.hogs}/>
 
       </div>
     )
