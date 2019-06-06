443e63e31b410f7d9be33b9b5b586b2dfd08912d
dnj547 <dnj547@live.com>
2019-06-06 14:48:13 -0400

    Add details to hog tiles

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,8 @@ import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
 import Hogs from './Hogs.js'
+
 class App extends Component {
-
-
-
   render() {
     return (
       <div className="App">
--- src/components/Hog.js
+++ src/components/Hog.js
@@ -2,18 +2,47 @@ import React, { Component } from 'react';
 
 export default class Hog extends Component {
 
+  state = {
+    details: false
+  }
 
+  details = (e) => {
+    // console.log(e.currentTarget);
+    // console.log(this);
+    this.setState({
+      details: true
+    })
+  }
+
+  showSomething = () => {
+    if (this.state.details === false) {
+      return <img src={require(`../hog-imgs/${this.props.hog.name.split(" ").map(word => {return word.toLowerCase()}).join("_")}.jpg`)} alt={this.props.hog.name}/>
+    } else {
+      return <div className="smallHeader">
+      Specialty: {this.props.hog.specialty}
+      <p></p>
+      Greased: {this.props.hog.greased}
+      <p></p>
+      Weight as a ratio: {this.props.hog['weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water']}
+      <p></p>
+      Highest medal achieved: 
+      {this.props.hog['highest medal achieved']}
+      </div>
+    }
+  }
 
   render() {
-
     return (
-      <div className="pigTile">
+      <div key={this.props.hog.name}
+        id={this.props.hog.name}
+        className="pigTile"
+        onClick={this.details}>
 
         <h3>{this.props.hog.name}</h3>
 
-         <img src={require(`../hog-imgs/${this.props.hog.name.split(" ").map(word => {
-           return word.toLowerCase()
-        }).join("_")}.jpg`)} alt={this.props.hog.name}/>
+        {this.showSomething()}
+
+
       </div>
     )
   }
